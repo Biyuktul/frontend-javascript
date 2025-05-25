cbe315ca7e25b351e6f183c56fe9581e1091dee8
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-25 14:48:49 +0300

    adding new methods

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -7,7 +7,7 @@ interface DirectorInterface {
 interface TeacherInterface {
     workFromHome() : string;
     getCoffeeBreak() : string;
-    WorkDirecotorTasks() : string;
+    WorkTeacherTasks() : string;
 }
 
 class Director implements DirectorInterface {
@@ -29,7 +29,7 @@ class Teacher implements TeacherInterface{
     getCoffeeBreak() : string {
       return "Cannot have a break"
     }
-    WorkDirecotorTasks() : string {
+    WorkTeacherTasks() : string {
       return "Getting to work"
     }
 }
@@ -39,4 +39,28 @@ function createEmployee (salary : number | string) : Director | Teacher {
     return new Teacher();
   }
   return new Director();
+}
+
+
+function isDirector(employee: Teacher | Director): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Teacher | Director) {
+  if (isDirector(employee)) {
+    employee.WorkDirecotorTasks();
+  } else {
+    employee.WorkTeacherTasks();
+  }
+}
+
+type Subjects = "Math" | "History";
+
+function teachClass(todayClass: Subjects): string {
+  if (todayClass === "Math") {
+    return "Teaching Math";
+  } else if (todayClass === "History") {
+    return "Teaching History";
+  }
+  return "";
 }
