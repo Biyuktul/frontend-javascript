3e9a9c5478fb1a40dfaca6974122f5497c389eaa
Yonatan <107502940+Biyuktul@users.noreply.github.com>
2025-05-25 12:55:47 +0300

    adding new classes

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -1,9 +1,3 @@
-// firstName(string) and lastName(string). These two attributes should only be modifiable when a Teacher is first initialized
-// fullTimeEmployee(boolean) this attribute should always be defined
-// yearsOfExperience(number) this attribute is optional
-// location(string) this attribute should always be defined
-// Add the possibility to add any attribute to the Object like contract(boolean) without specifying the name of the attribute
-
 interface Teacher{
     readonly firstName: string;
     readonly lastName: string;
@@ -11,4 +5,81 @@ interface Teacher{
     yearsOfExperience?: number;
     Location: string;
     [key: string]: any;
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction{
+    (firstName: string, lastName: string) : string;
+}
+
+const printTeacher : printTeacherFunction = (firstName, lastName) => {
+    let fullName = firstName[0] + " " + lastName;
+    return fullName;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  constructor(private firstName: string, private lastName: string) {}
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+interface DirectorInterface {
+    workFromHome() : string;
+    getCoffeeBreak() : string;
+    WorkDirecotorTasks() : string;
+}
+
+interface TeacherInterface {
+    workFromHome() : string;
+    getCoffeeBreak() : string;
+    WorkDirecotorTasks() : string;
+}
+
+class Director implements DirectorInterface {
+    workFromHome(): string {
+      return "Working from home"
+    }
+    getCoffeeBreak() : string {
+      return "Getting a coffee break"
+    }
+    WorkDirecotorTasks() : string {
+      return "Getting to director tasks"
+    }
+}
+
+class Teacher implements TeacherInterface{
+    workFromHome(): string {
+      return "Cannot work from home"
+    }
+    getCoffeeBreak() : string {
+      return "Cannot have a break"
+    }
+    WorkDirecotorTasks() : string {
+      return "Getting to work"
+    }
+}
+
+function createEmployee (salary : number | string) : Director | Teacher {
+  if (typeof salary == "number" && salary < 500){
+    return new Teacher();
+  }
+  return new Director();
 }
